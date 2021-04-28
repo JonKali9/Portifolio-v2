36d42d2508d52edf5b974106a5075c1b6d7160d9
Anton Devi <anton-devi@localhost.localdomain>
2021-04-28 23:44:12 +0200

    Updated welcome page animation

--- JS Files/welcome.js
+++ JS Files/welcome.js
@@ -1,14 +1,81 @@
-const title = document.querySelector('#content h1');
+const text = document.getElementById('text');
+const underscore = document.getElementById('underscore');
 const button = document.querySelector('#content button');
 
+//Function to write name
+const writeName = speed => {
+    setTimeout(() => {
+        text.innerHTML = './J';
+        setTimeout(() => {
+            text.innerHTML = './Jo';
+            setTimeout(() => {
+                text.innerHTML = './Jon';
+                setTimeout(() => {
+                    text.innerHTML = './JonK';
+                    setTimeout(() => {
+                        text.innerHTML = './JonKa';
+                        setTimeout(() => {
+                            text.innerHTML = './JonKal';
+                            setTimeout(() => {
+                                text.innerHTML = './JonKali';
+                            }, speed);
+                        }, speed);
+                    }, speed);
+                }, speed);
+            }, speed);
+        }, speed);
+    }, speed);
+}
+//Function to clear name
+const clearName = speed => {
+    setTimeout(() => {
+        text.innerHTML = './JonKali';
+        setTimeout(() => {
+            text.innerHTML = './JonKal';
+            setTimeout(() => {
+                text.innerHTML = './JonKa';
+                setTimeout(() => {
+                    text.innerHTML = './JonK';
+                    setTimeout(() => {
+                        text.innerHTML = './Jon';
+                        setTimeout(() => {
+                            text.innerHTML = './Jo';
+                            setTimeout(() => {
+                                text.innerHTML = './J';
+                                setTimeout(() => {
+                                    text.innerHTML = './';
+                                    }, speed);
+                                }, speed);
+                            }, speed);
+                        }, speed);
+                    }, speed);
+                }, speed);
+            }, speed);
+        }, speed);
+}
+
+//Actually do the animation
+setTimeout(() => {
+    writeName(250);
+    setTimeout(() => {
+        clearName(300);
+    }, 6500);
+}, 1000);
+
 setInterval(() => {
-    const lastChar = title.innerHTML[title.innerHTML.length-1];
-    if (lastChar === '_') {
-        title.innerHTML = title.innerHTML.substring(0, title.innerHTML.length-1);
-    } else {
-        title.innerHTML += '_';
-    }
-}, 600)
+    setTimeout(() => {
+        writeName(250);
+        setTimeout(() => {
+            clearName(300);
+        }, 6500);
+    }, 500);
+}, 12500);
+
+//Animation for the underscore
+setInterval(() => {
+    if (underscore.innerHTML === '_') underscore.innerHTML = '';
+    else underscore.innerHTML = '_';
+}, 800);
 
 button.addEventListener('click', event => {
     window.location = 'about-me.html';
