d74f5de3b341043ccb13d10bf0d488d9ba7ffe74
Anton Devi <anton-devi@localhost.localdomain>
2021-04-28 23:09:40 +0200

    Finished Project page

--- JS Files/projects.js
+++ JS Files/projects.js
@@ -1,6 +1,16 @@
 const button = document.querySelector('#ui button');
+const siteLinks = document.getElementsByClassName('live-site');
+const githubRepos = document.getElementsByClassName('github-repo');
 
 //Make button functional
 button.addEventListener('click', event => {
     window.location = 'info.html';
 })
+
+//Customize "live site" and "github repo" elements
+siteLinks[3].style.bottom = '0';
+githubRepos[3].style.bottom = '0';
+siteLinks[4].style.bottom = '0';
+githubRepos[4].style.bottom = '0';
+siteLinks[5].style.bottom = '0';
+githubRepos[5].style.bottom = '0';
