84f964438f99980c2d0a15a2628bd779de784104
Anton Devi <anton-devi@localhost.localdomain>
2021-04-29 21:07:46 +0200

    Added to project pages so that the links only appear when you hover over a project

--- JS Files/projects.js
+++ JS Files/projects.js
@@ -1,4 +1,5 @@
 const button = document.querySelector('#ui button');
+const projects = document.getElementsByClassName('project');
 const siteLinks = document.getElementsByClassName('live-site');
 const githubRepos = document.getElementsByClassName('github-repo');
 
@@ -14,3 +15,14 @@ siteLinks[4].style.bottom = '0';
 githubRepos[4].style.bottom = '0';
 siteLinks[5].style.bottom = '0';
 githubRepos[5].style.bottom = '0';
+
+for (let i=0; i<projects.length; i++) {
+    projects[i].addEventListener('mouseover', () => {
+        siteLinks[i].style.display = 'initial';
+        githubRepos[i].style.display = 'initial';
+    })
+    projects[i].addEventListener('mouseout', () => {
+        siteLinks[i].style.display = 'none';
+        githubRepos[i].style.display = 'none';
+    })
+}
